f7c7f1f218ef638236e6bb9834a71eb1f24fbdbb
MigalejoLejo <migalejo85@gmail.com>
2023-05-11 16:08:37 +0100

    - error on email validation corrected

--- screens/signIn.js
+++ screens/signIn.js
@@ -37,7 +37,7 @@ function SignIn({ navigation }) {
         // console.log("password: ", password, isValidPassword)
     }, [password])
 
-    emailValidation = () => {
+    const emailValidation = () => {
         const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
         if (!email || regex.test(email) === false) {
             setIsValidEmail(false)
